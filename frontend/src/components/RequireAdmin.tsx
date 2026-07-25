6570b974b74d4808a12bb4e1afdf300c7a3f8add
jcharra <johannes.charra@gmail.com>
2026-07-25 18:21:00 +0200

    fix import

--- frontend/src/components/RequireAdmin.tsx
+++ frontend/src/components/RequireAdmin.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
+import type { JSX } from 'react/jsx-runtime'
 
 export default function RequireAdmin({ children }: { children: JSX.Element }) {
   const { user } = useAuth()
--- frontend/src/components/RequireAuth.tsx
+++ frontend/src/components/RequireAuth.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
+import type { JSX } from 'react/jsx-runtime'
 
 export default function RequireAuth({ children }: { children: JSX.Element }) {
   const { user } = useAuth()
